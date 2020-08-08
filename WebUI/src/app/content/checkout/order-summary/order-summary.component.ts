be5e39d9b7ac9d7878f1889e9ac056dedf075586
homestop <homehctop@gmail.com>
2020-08-08 08:10:17 +0300

    feat(Content): Moved order-summary logic to independent component.

---
+++ WebUI/src/app/content/checkout/order-summary/order-summary.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { SendHelper } from '../ helpers';
+import { Order } from 'src/app/core/models/order.model';
+import { Product } from 'src/app/core/models/product.model';
+import { OrderService } from 'src/app/core/services/order.service';
+import { CheckoutService } from 'src/app/core/services/checkout.service';
+
+@Component({
+  selector: 'app-order-summary',
+  templateUrl: './order-summary.component.html',
+  styleUrls: ['./order-summary.component.css']
+})
+export class OrderSummaryComponent implements OnInit {
+  constructor(
+    private orderService: OrderService,
+    private checkoutService: CheckoutService
+  ) { }
+
+  @Input() productList: Array<Product>;
+
+  onSendClick() {
+    const order = new Order();
+
+    SendHelper.processOrder(order, this.productList);
+    this.orderService.post(order).subscribe();
+    console.log(JSON.stringify(order));
+  }
+
+  onRemoveByIndex(product: Product) {
+    this.checkoutService.cartRemoveByIndex(product);
+  }
+
+  onRemoveAllClick() {
+    this.checkoutService.cartRemoveAll();
+  }
+
+  ngOnInit() {
+  }
+
+}
